eca5dddda6387d9854517d426faf1ef7aa8d67d6
Mukhtiar Ali <mukhtiar.g20505@iqra.edu.pk>
2025-05-22 05:23:34 +0500

    initial

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,6 +1,7 @@
 
 "use client";
 
+export const dynamic = 'force-dynamic';
 import { useState, useEffect, useCallback } from 'react';
 import { AppLayout } from "@/components/app-layout";
 import { ConversationList } from "@/components/messages/conversation-list";
